dccea4a77fd3aa0a23de54ba6d6db587aa7b4b84
COLE <coleazill@gmail.com>
2023-12-31 00:30:04 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HashVerify
+{
+    
+    public partial class Form1 : Form
+    {
+        private string selectedFilePath = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.ReadOnly = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFilePath))
+            {
+                textBox2.Text = "Please select a file before computing hashes.";
+                return;
+            }
+
+            try
+            {
+                string fileContent = File.ReadAllText(selectedFilePath);
+
+                if (radioButton1.Checked)
+                {
+                    string sha256Hash = RadioButtonMethods.ComputeSha256Hash(fileContent);
+                    textBox2.Text = sha256Hash;
+                }
+                else if (radioButton2.Checked)
+                {
+                    string md5Hash = RadioButtonMethods.ComputeMd5Hash(fileContent);
+                    textBox2.Text = md5Hash;
+                }
+                else if (radioButton4.Checked)
+                {
+                    string sha512 = RadioButtonMethods.ComputeSha512(fileContent); 
+                    textBox2.Text = sha512;
+                }
+                else
+                {
+                    textBox2.Text = "No hash algorithm selected";
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox2.Text = $"Error reading or hashing file: {ex.Message}";
+            }
+        }
+
+        // grabs the file name, and can be modified to hash a dir if needed.
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "All Files (*.*)|*.*"; 
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFilePath = openFileDialog.FileName;
+                    string fileName = Path.GetFileNameWithoutExtension(selectedFilePath);
+
+                    textBox1.Text = fileName;
+                }
+            }
+        }
+
+    }
+}
---
+++ RadioButtonMethods.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using NuGet.Packaging.Signing;
+
+namespace HashVerify
+{
+    // Hashing algorithms in seperate functions and can be modified
+    public class RadioButtonMethods
+    {
+        string filename = null;
+
+        public static string ComputeSha256Hash(string filename)
+        {
+            try
+            {
+                using (SHA256 sha256Hash = SHA256.Create())
+                {
+                    byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(filename));
+
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        builder.Append(bytes[i].ToString("X2"));
+                    }
+                    return builder.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static string ComputeMd5Hash(string filename)
+        {
+            try
+            {
+                using (MD5 md5 = MD5.Create())
+                {
+                    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(filename);
+                    byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        sb.Append(hashBytes[i].ToString("X2"));
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                return string.Empty;
+            }
+        }
+
+        public static string ComputeSha512(string filename)
+        {
+            try
+            {
+                using (SHA512 sha512 = SHA512.Create())
+                {
+                    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(filename);
+                    byte[] hashBytes = sha512.ComputeHash(inputBytes);
+
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; (i < hashBytes.Length); i++)
+                    {
+                        sb.Append(hashBytes[i].ToString("X2"));
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
